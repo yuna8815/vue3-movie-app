373f8f78b70f7d92206d0576e528736fbaa9de45
KimYuNa <hugyaa@gmail.com>
2022-07-18 21:23:18 +0900

    update - MovieList - Movie Items

---
+++ src/components/MovieList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <MovieItem v-for="movie in movies" :key="movie.imdbID" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieItem from '~/components/MovieItem'
+export default {
+  components: {
+    MovieItem
+  },
+  data() {
+    return {
+      movies: []
+    }
+  }
+}
+</script>
