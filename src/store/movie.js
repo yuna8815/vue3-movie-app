bc0ad76ad1e9000406eb854976bf9eabd1d457e7
KimYuNa <hugyaa@gmail.com>
2022-07-19 21:54:26 +0900

    검색 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -36,18 +36,36 @@ export default {
   actions: {
     // Exam
     // async searchMovies(context, payload) {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload
       const OMDB_API_KEY = '7035c60c';
 
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type="${type}&y=${year}&page=1`);
-      const { Search, totalResult } = res.data
+      const { Search, totalResults } = res.data
       // context.commit('updateState', {
       commit('updateState', {
         movies: Search,
         // messaage: 'Hello world!',
         // loading: true
       })
+      // console.log(totalResults, typeof totalResults)
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      // 추가 요청
+      if(pageLength > 1) {
+        for(let page = 2; page <= pageLength; page++) {
+          // number
+          if(page > (number / 10)) break;
+          
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type="${type}&y=${year}&page=${page}`);
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          })
+        }
+      }
     }
   }
 }
