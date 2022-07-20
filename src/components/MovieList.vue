3dfc14ba67fbf325fa42d770ac1280a276e5a7ea
yuna8815 <yuna.kim@smd-sol.com>
2022-07-20 14:13:13 +0900

    영화 아이템 출력

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -2,10 +2,13 @@
   <div class="container">
     <div class="inner">
       <div class="message">{{ message }}</div>
-      <MovieItem
-        v-for="movie in movies"
-        :key="movie.imdbID"
-        :movie="movie" />
+
+      <div class="movies">
+        <MovieItem
+          v-for="movie in movies"
+          :key="movie.imdbID"
+          :movie="movie" />
+      </div>
     </div>
   </div>
 </template>
@@ -27,3 +30,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  .movies {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+</style>
