564a50662f63aba16e7d95c3a6b780d402c91377
yuna8815 <yuna.kim@smd-sol.com>
2022-07-20 14:20:10 +0900

    container 너비 지정

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -32,7 +32,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '~/scss/main';
+
 .container {
+  margin-top: 30px;
+  .inner {
+    background-color: $gray-200;
+    padding: 10px 0;
+    border-radius: 4px;;
+  }
   .movies {
     display: flex;
     flex-wrap: wrap;
