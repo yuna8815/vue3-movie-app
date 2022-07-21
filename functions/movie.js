525bfb613b1d68e770ccafc24391c4a2136a900a
yuna8815 <yuna.kim@smd-sol.com>
2022-07-21 16:24:00 +0900

    환경 변수 구성

--- functions/movie.js
+++ functions/movie.js
@@ -1,10 +1,13 @@
 const axios = require('axios')
+// const OMDB_API_KEY = process.env.OMDB_API_KEY
+const { OMDB_API_KEY } = process.env
 
-exports.handler = async function(event, context) {
+exports.handler = async function(event) {
   console.log(event)
   const payload = JSON.parse(event.body)
   const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c';
+  // 보안상의 문제로 환경변수로 변경
+  // const OMDB_API_KEY = '7035c60c';
   const url = id
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type="${type}&y=${year}&page=${page}`
